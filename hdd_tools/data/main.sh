2de40ce9165c97a2d3fa69d27ade31528badc851
Michal M <michal.mydlarz@gmail.com>
2020-05-11 21:21:05 +0200

    Added SMART data as attributes

--- hdd_tools/data/main.sh
+++ hdd_tools/data/main.sh
@@ -3,12 +3,17 @@ CONFIG_PATH=/data/options.json
 HDD_PATH="$(jq --raw-output '.hdd_path' $CONFIG_PATH)"
 OUTPUT_FILE="$(jq --raw-output '.output_file' $CONFIG_PATH)"
 
-TEMP=$(/usr/sbin/smartctl -a ${HDD_PATH} | grep Temperature_Celsius | awk '{print $(NF)}')
+SMARTCTL_OUTPUT=$(/usr/sbin/smartctl -a $HDD_PATH)
+echo "$SMARTCTL_OUTPUT" > /share/hdd_tools/${OUTPUT_FILE}
 
-echo "[$(date)][Info] HDD Temp: $TEMP°" > /proc/1/fd/1 2>/proc/1/fd/2
-echo "[$(date)][Info] HDD Temp: $TEMP°" >> /share/hdd_tools/${OUTPUT_FILE}
+ATTRIBUTES=$(echo "$SMARTCTL_OUTPUT" | egrep -o '^[0-9 ]+.*[0-9]+$' | awk '{print "\"" $2 "\":\"" $(NF) "\"," }' | awk '{print tolower($0)}' | tr -d '\n') 
+MAIN_VALUE=$(echo "$SMARTCTL_OUTPUT" | grep Temperature_Celsius | awk '{print $(NF)}')
+API_CALL_BODY='{"state": "'"$MAIN_VALUE"'", "attributes": {"unit_of_measurement":"°C","friendly_name":"Hdd Temp",'"${ATTRIBUTES::-1}"'}}'
+
+echo "[$(date)][Info] Sensor value: $MAIN_VALUE°" > /proc/1/fd/1 2>/proc/1/fd/2
+echo "[$(date)][Info] API call body: $API_CALL_BODY" > /proc/1/fd/1 2>/proc/1/fd/2
 
 curl -X POST -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" \
        -H "Content-Type: application/json" \
-       -d $'{"state": "'"$TEMP"'", "attributes": {"unit_of_measurement": "°C", "friendly_name": "Hdd Temp"}}' \
+       -d "$API_CALL_BODY" \
        http://supervisor/core/api/states/sensor.hdd_temp
