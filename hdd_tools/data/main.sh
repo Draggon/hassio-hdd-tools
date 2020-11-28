249e48e259c6be029d33a724d828cea39fdff549
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2020-11-28 22:22:46 +0100

    qa: add `debug` configuration to avoid unnecessary disk writes
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- hdd_tools/data/main.sh
+++ hdd_tools/data/main.sh
@@ -1,17 +1,21 @@
 CONFIG_PATH=/data/options.json
 
 HDD_PATH="$(jq --raw-output '.hdd_path' $CONFIG_PATH)"
+DEBUG="$(jq --raw-output '.debug' $CONFIG_PATH)"
 OUTPUT_FILE="$(jq --raw-output '.output_file' $CONFIG_PATH)"
 ATTRIBUTES_PROPERTY="$(jq --raw-output '.attributes_property' $CONFIG_PATH)"
 
 SMARTCTL_OUTPUT=$(/usr/sbin/smartctl -a $HDD_PATH --json)
-echo "$SMARTCTL_OUTPUT" > /share/hdd_tools/${OUTPUT_FILE}
+
+if [ "$DEBUG" = "true" ]; then
+    echo "$SMARTCTL_OUTPUT" > /share/hdd_tools/${OUTPUT_FILE}
+fi
 
 CURRENT_TEMPERATURE=$(echo $SMARTCTL_OUTPUT | jq --raw-output '.temperature.current')
 SENSOR_DATA='{"state": "'"$CURRENT_TEMPERATURE"'", "attributes": {"unit_of_measurement":"°C","friendly_name":"HDD Temperature"}}'
 
 if ! [ -z "$ATTRIBUTES_PROPERTY" ]; then
-    ATTRIBUTES=$(echo $SMARTCTL_OUTPUT | jq -e --raw-output '.$ATTRIBUTES_PROPERTY' || echo "{}")
+    ATTRIBUTES=$(echo $SMARTCTL_OUTPUT | jq -e --raw-output ".${ATTRIBUTES_PROPERTY}" || echo "{}")
     SENSOR_DATA=$(echo $SENSOR_DATA | jq -n ".attributes += $ATTRIBUTES")
 fi
 
