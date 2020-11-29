4081ad606dafe1ccfcdc8678ef0bb1756339680e
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2020-11-29 12:46:00 +0100

    feat: introduce `attributes_format`
    Can be one of `list` or `object` while `object` is default.
    
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- hdd_tools/data/main.sh
+++ hdd_tools/data/main.sh
@@ -6,13 +6,9 @@ HDD_PATH="$(jq --raw-output '.hdd_path' $CONFIG_PATH)"
 DEBUG="$(jq --raw-output '.debug' $CONFIG_PATH)"
 OUTPUT_FILE="$(jq --raw-output '.output_file' $CONFIG_PATH)"
 ATTRIBUTES_PROPERTY="$(jq --raw-output '.attributes_property' $CONFIG_PATH)"
+ATTRIBUTES_FORMAT="$(jq --raw-output '.attributes_format' $CONFIG_PATH)"
 
 SMARTCTL_OUTPUT=$(/usr/sbin/smartctl -a $HDD_PATH --json)
-
-if [ -z "$SENSOR_NAME" ]; then
-    echo "Please check your configuration. sensor_name seems to be missing!"
-    exit 1;
-fi
 
 if [ "$DEBUG" = "true" ]; then
     echo "$SMARTCTL_OUTPUT" > /share/hdd_tools/${OUTPUT_FILE}
@@ -23,15 +19,34 @@ SENSOR_DATA='{"state": "'"$CURRENT_TEMPERATURE"'", "attributes": {"unit_of_measu
 
 if ! [ -z "$ATTRIBUTES_PROPERTY" ]; then
     ATTRIBUTES=$(echo $SMARTCTL_OUTPUT | jq -e --raw-output ".${ATTRIBUTES_PROPERTY}" || echo "{}")
+
+    case "$ATTRIBUTES_FORMAT" in
+        object)
+        ;;
+        list)
+            ATTRIBUTES=$(echo $ATTRIBUTES | jq 'map({(.name): .raw.string | capture("^(?<value>[[:digit:]]+)").value | tonumber}) | add | with_entries(.key |= ascii_downcase)')
+        ;;
+        *)
+            echo "[$(date)][ERROR] Unsupported attributes format \"$ATTRIBUTES_FORMAT\" given!"
+            exit 1;
+        ;;
+    esac
+
     SENSOR_DATA=$(echo $SENSOR_DATA | jq ".attributes += $ATTRIBUTES")
 fi
 
-echo "[$(date)][Info] Sensor value: $CURRENT_TEMPERATURE°"
+echo "[$(date)][INFO] Sensor value: ${CURRENT_TEMPERATURE}°"
+
+if [ "$DEBUG" = "true" ]; then
+    echo "[$(date)][DEBUG] Sensor data which would be pushed to home-assistant and exposed as \"$SENSOR_NAME\": $SENSOR_DATA"
+    echo "[$(date)][DEBUG] debug is enabled, sensor data is not published to home-assistant!"
+    exit 0;
+fi
 
 curl -X POST -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" \
        -s \
        -o /dev/null \
        -H "Content-Type: application/json" \
        -d "$SENSOR_DATA" \
-       -w "[$(date)][Info] Sensor update response code: %{http_code}\n" \
+       -w "[$(date)][INFO] Sensor update response code: %{http_code}\n" \
        http://supervisor/core/api/states/${SENSOR_NAME}
